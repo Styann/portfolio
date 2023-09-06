0ba2929dd06a548c1876b34f79f2219b8824e7a4
Styann <steunouyann44@gmail.com>
2023-09-06 21:00:45 +0200

    portfolio V2

---
+++ index.js
@@ -0,0 +1,18 @@
+function GoToGitHub(){
+    window.open('https://github.com/Styann', '_blank');
+}
+
+function GoToLinkedIn(){
+    window.open('https://www.linkedin.com/in/yann-steunou-b72292228/', '_blank');
+}
+
+function goToGitHubProject(project){
+    window.open(`https://github.com/Styann/${project}`, '_blank');
+}
+
+function getOld(birthdate = new Date('12/11/2002')){  
+    var ageDifMs = Date.now() - birthdate.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
